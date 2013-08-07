bbc8e20042990fd427020921b24ddc7b3d603e07
Jared Smith <jared.rit@me.com>
2013-08-07 10:02:24 -0700

    Add testfile

---
+++ test/employeeproviderSpec.js
@@ -0,0 +1,9 @@
+var assert = require("assert");
+var EmployeeProvider = require('./employeeprovider').EmployeeProvider;
+describe('EmployeeProvider', function() {
+	describe('#getCollection()', function() {
+		it('should return a collection of employees', function() {
+
+		})
+	})
+})
