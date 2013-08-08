8c0aef9f7a65a8c37a747572c15fb0937cedc0d5
Jared Smith <jared.rit@me.com>
2013-08-07 21:57:30 -0700

    Change name of testfiles

--- test/employeeproviderSpec.js
+++
@@ -1,9 +0,0 @@
-var assert = require("assert");
-var EmployeeProvider = require('./employeeprovider').EmployeeProvider;
-describe('EmployeeProvider', function() {
-	describe('#getCollection()', function() {
-		it('should return a collection of employees', function() {
-
-		})
-	})
-})
